e785aeec3faaa0546d533018df1e1db06a2e99d3
Elise Eijkhout <99038787@mydavinci.nl>
2017-04-18 14:16:57 +0200

    Add files via upload
    Done :)

---
+++ Kalender/view/kalender/create.php
@@ -0,0 +1,14 @@
+<div class="container">
+<h1>Create</h1>
+	<form action="<?= URL ?>kalender/createSave" method="post">
+	
+		<input type="text" name="person" placeholder="naam">
+		<input type="text" name="day" placeholder="dag">
+		<input type="text" name="month" placeholder="maand">
+		<input type="text" name="year" placeholder="jaar">
+
+		<input type="submit" value="Verzenden">
+	
+	</form>
+
+</div>
---
+++ Kalender/view/kalender/edit.php
@@ -0,0 +1,16 @@
+
+<div class="container">
+	<h1>Verjaardag bewerken</h1>
+	<form action="<?= URL ?>kalender/editSave" method="post">
+	
+		<p><input type="text" name="person" value="<?= $birthday['person']; ?>"></p>
+		<p><input type="text" name="day" value="<?= $birthday['day']; ?>"></p>
+		<p><input type="text" name="month" value="<?= $birthday['month']; ?>"></p>
+		<p><input type="text" name="year" value="<?= $birthday['year']; ?>"></p>
+
+		<input type="hidden" name="id" value="<?= $birthday['id']; ?>">
+		<input type="submit" value="Verzenden">
+	
+	</form>
+
+</div>
